4389b5e44818f09ebb65dcde23423043b7d9f170
Rahat Chowdhury <rczisan@gmail.com>
2023-09-15 12:41:08 +0600

    selected all the necessary fields & buttons in JS

---
+++ main.js
@@ -0,0 +1,9 @@
+// selection of all the necessary buttons & textfields
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operation]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const equalsButton = document.querySelector('[data-equals]');
+const previousOperandTextField = document.querySelector('[data-previous-operand]');
+const currentOperandTextField = document.querySelector('[data-current-operand]');
+
