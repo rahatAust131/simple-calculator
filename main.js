a69d3ace901ed9f1f0e1776fc623b65211c6d91b
Rahat Chowdhury <rczisan@gmail.com>
2023-09-20 23:32:47 +0600

    selected all the necessary fields & buttons in JS

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
+class Calculator {
+  constructor(previousOperandTextField, currentOperandTextField) {
+    this.previousOperandTextField = previousOperandTextField;
+    this.currentOperandTextField = currentOperandTextField;
+    this.clear();
+  }
+
+  clear() {
+    this.currentOperandTextField = "";
+    this.previousOperandTextField = "";
+    this.operation = undefined;
+  }
+
+  delete() {
+
+  }
+
+  appendNumber(number) {
+
+  }
+
+  chooseOperation(operation) {
+
+  }
+
+  calculate() {
+
+  }
+
+  updateDisplay() {
+
+  }
+}
+
 // selection of all the necessary buttons & textfields
 const numberButtons = document.querySelectorAll('[data-number]');
 const operatorButtons = document.querySelectorAll('[data-operation]');
